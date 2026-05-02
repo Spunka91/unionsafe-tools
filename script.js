4418ce3c9d8ee469c65a649b4aa6a17092f60d98
Spunka91 <jscmins@gmail.com>
2026-05-02 03:05:28 -0400

    Add loading alert to script execution
    Added a loading alert when the script is executed.

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contactForm");
-
-  if (!form) return;
-
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const formData = {
-      name: form.name.value,
-      email: form.email.value,
-      organization: form.organization.value,
-      mailing_list: form.mailing_list.checked ? "Yes" : "No",
-      message: form.message.value
-    };
-
-    fetch(https://script.google.com/macros/s/AKfycbwYxk-n5C7LnhtT3g1CTEX3HxoH-PSBK8xVYC5VHLw7CzWJFzBq0k6FjwPeDcWK6pqo/exec, {
-      method: "POST",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then(() => {
-      alert("Thank you! Submission received.");
-      form.reset();
-    })
-    .catch(() => {
-      alert("Submission failed.");
-    });
-  });
+  alert("SCRIPT IS LOADING");
 });
